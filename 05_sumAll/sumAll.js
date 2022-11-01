1bc0eaef3fc93cf2c1d707cd11f71293b3cfc9c3
Castle <codelikecastle@gmail.com>
2022-10-31 23:26:07 -0500

    Update function/tests as instructed in README.md

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function(startingValue, endingValue) {
+    let finalSum = 0;
+    const array = [startingValue, endingValue].sort(function(a, b){return a - b});
 
+    if (startingValue < 0 || endingValue < 0 || typeof startingValue != "number" || typeof endingValue != "number") {
+        return "ERROR"
+    } else {
+    for (let i = array[0]; i <= array[1]; i++) {
+        finalSum += i;
+    }
+    return finalSum;
+}
 };
 
 // Do not edit below this line
