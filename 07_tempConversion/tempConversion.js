d2c6a87a47a4e54df194b01c3c5a51759e6cfa79
Castle <codelikecastle@gmail.com>
2022-11-01 11:23:39 -0500

    Update functions/tests as instructed by README.md

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,6 @@
-const ftoc = function() {
+const ftoc = (tempInF) => parseFloat(((((tempInF - 32) * (0.5556)) * 10) / 10 ).toFixed(1))
 
-};
-
-const ctof = function() {
-
-};
+const ctof = (tempInC) => parseFloat((((((tempInC * 1.8) + 32) * 10 / 10))).toFixed(1));
 
 // Do not edit below this line
 module.exports = {
