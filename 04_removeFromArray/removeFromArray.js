2e89620e0df251455a4fb8647e50ef0bb601829e
Castle <codelikecastle@gmail.com>
2022-10-31 22:51:02 -0500

    Update function/test as instructed via README.md

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...toBeRemoved) {
+    
+    const arrayOfArguments = Array.from(toBeRemoved);
+    let newArray = [];
 
+    for (let value of array) {
+        if (!arrayOfArguments.includes(value)) {
+            newArray.push(value);
+        }
+    }
+    return newArray
 };
 
 // Do not edit below this line
