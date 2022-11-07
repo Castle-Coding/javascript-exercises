c504c02ad227a4f87e0bf3435a693cf59f8a44de
Castle <codelikecastle@gmail.com>
2022-11-07 12:40:28 -0600

    Update functions/tests as instrcuted by README.md

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
+const add = function(a, b) {
+  let result = a + b;
+  return result;
 	
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	let result = a - b;
+  return result;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let total  = 0;
+  array.forEach(element => (total += element));
+  return total;
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  let total = 0;
+  total = array[0];
+  for (let i = 1; i < array.length; i++) {
+    total *= array[i]
+  };
+  return total;
+}
 
+const power = function(a, b) {
+  let total = 0;
+  total = a ** b
+  return total;
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(number) {
+  let total = 1;
+  for (let i = 1; i <= number; i++) {
+    total *= i;
+  }
+	return total;
 };
 
 // Do not edit below this line
