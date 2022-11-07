81e222d734574fc64c21d9effd48e0c186d68116
Castle <codelikecastle@gmail.com>
2022-11-07 14:15:53 -0600

    Update function and tests as instructed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,15 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let input = string.replace(/[\.,-\/#!$%\^&\*;:{}=\-_`~()@\+\?><\[\]\+]/g, '')
+    input = input.replace(/\s+/g, '')
+    input = input.toLowerCase();
+    let palindromeTest = Array.from(input);
+    palindromeTest = palindromeTest.reverse()
+    input = Array.from(input);
+    if (input.toString() == palindromeTest.toString()) {
+        return true;
+    } else {
+        return false;
+    }
 
 };
 
