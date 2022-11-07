d225a5f0b1a28874a9b76b4c8c191a69f77036f4
Castle <codelikecastle@gmail.com>
2022-11-07 16:05:25 -0600

    Update functions to pass tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,29 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+   const oldestPerson = people.reduce(function (personA, personB) {
+        
+        let personAAge = 0;
+        let personBAge = 0;
 
+        if (personA.hasOwnProperty('yearOfDeath')) {
+            personAAge = personA.yearOfDeath - personA.yearOfBirth;
+        } else {
+            personAAge = (new Date().getFullYear()) - personA.yearOfBirth;            
+        }
+
+        if (personB.hasOwnProperty('yearOfDeath')) {
+            personBAge = personB.yearOfDeath - personB.yearOfBirth
+        } else {
+            personBAge = (new Date().getFullYear()) - personB.yearOfBirth
+        }
+
+        if(personAAge > personBAge) {
+            return personA;
+        } else {
+            return personB;
+        }
+
+   }, people[people.length - 1])
+   return (oldestPerson);
 };
 
 // Do not edit below this line
