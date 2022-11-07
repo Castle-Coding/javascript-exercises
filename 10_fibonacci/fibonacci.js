0d16ec059484cb47118174cc5f9d5ac8097496ed
Castle <codelikecastle@gmail.com>
2022-11-07 14:45:23 -0600

    Update functions to pass tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function (indexOfFib) {
+    indexOfFib = parseInt(indexOfFib)
 
+    if (typeof indexOfFib == "number" && indexOfFib > 0) {
+        let result = [1, 1];
+
+        for (let i = 1; i < parseInt(indexOfFib); i++) {
+            result.push(result[i - 1] + result[i]);
+
+        }
+        return (result[indexOfFib - 1]);
+    } else {
+        return ("OOPS")
+    }
 };
 
 // Do not edit below this line
